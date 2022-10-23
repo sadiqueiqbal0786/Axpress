6f6d50f4bd184d855004a4b6946533126b3da37c
sadiqueiqbal0786 <43597624+sadiqueiqbal0786@users.noreply.github.com>
2022-10-24 00:11:08 +0530

    23-10-2022

--- app/src/main/java/com/example/axepress/Adapters/usersAdapter.kt
+++ app/src/main/java/com/example/axepress/Adapters/usersAdapter.kt
@@ -12,6 +12,11 @@ import com.example.axepress.Adapters.usersAdapter.ViewHolder
 import com.example.axepress.ChatDetailedActivity
 import com.example.axepress.Models.Users
 import com.example.axepress.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
 
 class usersAdapter(var list:ArrayList<Users>,var context: Context?):
@@ -25,6 +30,26 @@ class usersAdapter(var list:ArrayList<Users>,var context: Context?):
         var user:Users= list.get(position)
         Picasso.get().load(user.getprofilepic()).placeholder(R.mipmap.user).into(holder.image)
         holder.userName?.text = user.getUsername()
+
+        FirebaseDatabase.getInstance().reference.child("Chats")
+            .child(FirebaseAuth.getInstance().uid+user.getUserId())
+            .orderByChild("timeStamp")
+            .limitToLast(1)
+            .addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                  if (snapshot.hasChildren()){
+                      for (snapshot1:DataSnapshot in snapshot.children){
+                          holder.lastMessage?.text=snapshot1.child("message").value.toString()
+                      }
+                  }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
 
         holder.itemView.setOnClickListener{
             val intent= Intent(context,ChatDetailedActivity::class.java)
